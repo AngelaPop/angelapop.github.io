6edf93bf4de5f5555059fa38e9e21a6cf960a815
AngelaPop <44302246+AngelaPop@users.noreply.github.com>
2018-11-15 15:07:54 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+$('#myBtn').click(function(){
+    $('#myBtn').animate({
+        scrollTop: 0
+    }, 2000);
+});
+
